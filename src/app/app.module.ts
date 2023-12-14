cbc2bd5da5a34cda84974316932a93e675232be4
athuls8991@gmail.com <athuls8991@gmail.com>
2023-12-14 12:14:22 +0530

    add search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TableLeftComponent } from './table-left/table-left.component';
 import { TableRightComponent } from './table-right/table-right.component';
+import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TableLeftComponent,
-    TableRightComponent
+    TableRightComponent,
+    HomeComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
